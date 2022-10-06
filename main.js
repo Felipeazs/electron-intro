58034cf836e1a50fe2badaa4fc2d3eedddf0831a
felipeazs <felipeazs@gmail.com>
2022-10-06 14:47:16 -0300

    image preferences, about page and output directory

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 const path = require('path')
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu } = require('electron')
 
 const isDev = process.env.NODE_ENV !== 'production'
 const isMac = process.platform === 'darwin'
 
+//Create a window
 function createMainWindow() {
     const mainWindow = new BrowserWindow({
         title: 'Image Resizer',
         width: isDev ? 1000 : 500,
         height: 600,
+
+        webPreferences: {
+            contextIsolation: true,
+            nodeIntegration: true,
+            preload: path.join(__dirname, 'preload.js')
+        }
     })
 
     if (isDev) {
@@ -18,8 +25,24 @@ function createMainWindow() {
     mainWindow.loadFile(path.join(__dirname, './renderer/index.html'))
 }
 
+// Create about window
+function createAboutWindow() {
+
+    const aboutWindow = new BrowserWindow({
+        title: 'About Image Resizer',
+        width: 300,
+        height: 300,
+    })
+    aboutWindow.loadFile(path.join(__dirname, './renderer/about.html'))
+}
+
+
 app.whenReady().then(() => {
     createMainWindow()
+
+
+    const mainMenu = Menu.buildFromTemplate(menu)
+    Menu.setApplicationMenu(mainMenu)
 
     app.on('active', () => {
         if (BrowserWindow.getAllWindows().length === 0) {
@@ -27,6 +50,29 @@ app.whenReady().then(() => {
         }
     })
 })
+
+// menu template
+const menu = [
+    ...(isMac ? [{    //on mac
+        label: app.name,
+        submenu: [{
+            label: 'About',
+            click: createAboutWindow
+
+        }]
+    }] : []),
+    {
+        role: 'fileMenu' // Creates a quit menu
+    },
+    ...(!isMac ? [{   //on Windows
+        label: "Help",
+        submenu: [{
+            label: "About",
+            click: createAboutWindow
+        }],
+    }] : [])
+]
+
 
 app.on('window-all-closed', () => {
     if (!isMac) {
